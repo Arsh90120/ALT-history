8ec282c8ee092f73afe7dab249b768f2cdadefca
Arsh90120 <scarletspeedster2131@gmail.com>
2026-02-11 17:17:31 -0500

    Add era-specific data attribute for dynamic UI theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { GameProvider } from './context/GameContext'
+import { useEffect } from 'react'
+import { GameProvider, useGame } from './context/GameContext'
 import Landing from './pages/Landing'
 import EraSelection from './pages/EraSelection'
 import CountrySelection from './pages/CountrySelection'
 import Game from './pages/Game'
 
+function AppContent() {
+  const { state } = useGame()
+
+  // Update data-era attribute on document element when era changes
+  useEffect(() => {
+    if (state.era) {
+      document.documentElement.setAttribute('data-era', state.era)
+    } else {
+      document.documentElement.removeAttribute('data-era')
+    }
+  }, [state.era])
+
+  return (
+    <Router>
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/era" element={<EraSelection />} />
+          <Route path="/country" element={<CountrySelection />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
 function App() {
   return (
     <GameProvider>
-      <Router>
-        <div className="min-h-screen bg-dark text-light">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/era" element={<EraSelection />} />
-            <Route path="/country" element={<CountrySelection />} />
-            <Route path="/game" element={<Game />} />
-          </Routes>
-        </div>
-      </Router>
+      <AppContent />
     </GameProvider>
   )
 }
